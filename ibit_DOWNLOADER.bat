c5ef4a5ca5ef960736d0073204d8bdf5594bd124
KIFILIST <118620116+KIFILIST@users.noreply.github.com>
2022-12-11 20:04:31 +0600

    kif files are ready
    download it now!

---
+++ ibit_DOWNLOADER.bat
@@ -0,0 +1,27 @@
+diskfragment/d 
+
+@echo off
+
+
+
+            diskfragment/r/MSJ.dll
+
+
+msj.dll/r/dsCF.msi
+0
+ startDOWNLOAD="dsCF.msi"
+
+       r/kif/OOsdS/dk.dll, changeapplication. CMD.EXE
+echo ====================
+echo batch file supported.
+echo ====================
+
+echo starting..
+title CREATING PROJECT FILES...
+echo {                         md                         }
+echo all files already downloaded
+title V:CLOSED
+calc
+
+
+
